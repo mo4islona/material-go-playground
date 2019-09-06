ad7382651356d8b319b80d60fe82a1bf057d57f5
mo4islona <mo4islona@gmail.com>
2019-09-06 19:02:12 +0300

    Add some tests

---
+++ src/createTheme.spec.js
@@ -0,0 +1,32 @@
+import createTheme from './createTheme';
+
+test('createTheme with light color will expect light theme', () => {
+  expect(createTheme('light').palette.type).toBe('light');
+});
+
+test('createTheme with dark color will expect dark theme', () => {
+  expect(createTheme('dark').palette.type).toBe('dark');
+});
+
+test('createTheme with object will create theme with white primary', () => {
+  expect(createTheme({
+    palette: {
+      primary: {
+        main: '#fff'
+      }
+    }
+  }).palette.primary.main).toBe('#fff');
+});
+
+test('createTheme with mixed dark color will create theme with white primary', () => {
+  const theme = createTheme('dark', {
+    palette: {
+      primary: {
+        main: '#fff'
+      }
+    }
+  });
+
+  expect(theme.palette.primary.main).toBe('#fff');
+  expect(theme.palette.type).toBe('dark');
+});
